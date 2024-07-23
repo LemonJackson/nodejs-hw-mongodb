ac4734306ee4dc00c5b4377839a8471acb53cbf9
LemonJackson <107397445+LemonJackson@users.noreply.github.com>
2024-07-23 15:44:38 +0300

    V2.00

--- src/server.js
+++ src/server.js
@@ -34,11 +34,15 @@ export const setupServer = () => {
         });
     });
 
-    app.get('/contacts/:contactId', async (req, res) => {
+    app.get('/contacts/:contactId', async (req, res, next) => {
         const { contactId } = req.params;
         const contact = await getContactById(contactId);
+        if (!contact) {
+            res.status(404).json({
+                message: 'Contact not found!',
+            })
+        }
         res.status(200).json({
-            status: 200,
             message: `Successfully found contact with id ${contactId}!`,
             data: contact,
         });
